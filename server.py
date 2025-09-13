45f6c5a6557d12b4597ea0088d11d5a5c47e54db
ArrowSlashArrow <153234615+ArrowSlashArrow@users.noreply.github.com>
2025-09-13 14:50:56 -0600

    reverted to commit before refactoring

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import asyncio, json, websockets, random, requests, base64, os, time, ssl, sys, hashlib
 
-PORT = 2096
-BIND_IP = "127.0.0.1"
 
 def read_or_create(path, fallback={}):
     data = fallback
@@ -20,36 +18,9 @@ config_file = "config.json"
 users = read_or_create(users_file)
 messages = read_or_create(messages_file, default_msgs)
 
-config_boilderplate = {
-    "channels": {
-        "general": {
-            "whitelist_enabled": False,
-            "whitelist": [],
-            "blacklist_enabled": True,
-            "blacklist": []
-        }
-    },
-    "banned_ips": [],
-    "banned_users": [],
-    "admins": []
-}
 config = read_or_create(config_file)
-connected_users = {}
-anonymous_users = []
 
-class ConnectedUser:
-    def __init__(self, session, ip, socket, loggedin, admin):
-        self.session = session
-        self.ip = ip
-        self.socket = socket
-        self.loggedin = loggedin
-        self.admin = admin
-        
-    async def send_json(self, json_data):
-        await self.socket.send(json.dumps(json_data))
-        
-    async def kick(self):
-        await self.socket.close(code=1000, reason=b"bye bye")
+connected_users = {}
 
 def get_whitelisted_channels(cfg, user):
     whitelisted = []
@@ -65,13 +36,7 @@ def random_session():
     return f"{random.randint(0, 2**256):x}"
 
 def save_users():
-    # this file stores the passwords only so i can manually encode it
-    file = "{"
-    for user, pw in users.items():
-        file += f"\n    \"{user}\": [\n        {pw[0]},\n        {pw[1]}\n    ],"
-    file = file[:-1]
-    file += "\n}"
-    open(users_file, "w").write(file)
+    json.dump(config, open(users_file, "w"))
 
 def save_config():
     json.dump(config, open(config_file, "w"), indent=4)
@@ -95,23 +60,23 @@ def connect_msg(user):
 def disconnect_msg(user):
     return {"user": user, "time": int(time.time() * 1000), "msg": "bye bye", "replying_to": "-1", "type": "Disconnect"}
 
+
 async def main(ws):
     async def send_json(data):
-        print(f"sending {data}")
         await ws.send(json.dumps(data))
         
     async def check_session(msg, what, check_logged_in=True):
-        if msg.get("session", "") != session or session == "":
+        if msg["session"] != session or session == "":
             print("bad session")
             await send_json({"success": False, "reason": "Bad session", "what": what})
             return True
-        if check_logged_in and not curr_user.loggedin:
+        if check_logged_in and not loggedin:
             await send_json({"success": False, "reason": "Not logged in", "what": what})
             return True
         return False
 
     async def check_session_admin(msg, what):
-        if msg["session"] != session or session == "" or not curr_user.admin:
+        if msg["session"] != session or session == "" or not admin:
             print("bad session")
             await send_json({"success": False, "reason": "Bad session", "what": what})
             return True
@@ -119,32 +84,40 @@ async def main(ws):
     
     async def broadcast_msg(msg):
         removelist = []    
-        for username, connection in connected_users.items():
+        for ip, connection in connected_users.items():
             try:
-                await connection.send_json(msg)
+                await connection["websocket"].send(json.dumps(msg))
             except Exception as e:
-                print(f"Failed to send message to {username}: {e}")
-                removelist.append(username)
+                print(f"bad happened: {e}")
+                removelist.append(ip)
                 
-        for user in removelist:
-            connected_users.pop(user, None)
+        for ip in removelist:
+            connected_users.pop(ip, None)
             
-    def get_user_info(username):
-        return connected_users.get(username)
+    def get_user_info(user):
+        user_info = {}
+        print(user, connected_users)
+        for ip, connected_user in connected_users.items():
+            if connected_user["user"] == user:
+                user_info = connected_user
+                user_info["ip"] = ip
+        return user_info
             
-    async def kick(username):
-        if username not in connected_users:
+    async def kick(user):
+        user_info = get_user_info(user)
+        if user_info == {}:
             await send_json({"success": False, "reason": "This user is not connected", "what": "kickattempt"})
             return
         
-        user_info = connected_users[username]
-        await user_info.kick()
+        print(user_info)
+        await user_info["websocket"].close(code=1000, reason=b"bye bye")
             
         channel = messages["channels"][0]
-        dc_msg = disconnect_msg(username)
+        ip = user_info["ip"]
+        dc_msg = disconnect_msg(user_info["user"])
         
-        print(f"{username} was kicked")
-        connected_users.pop(username, None)
+        print(f"{ip} was kicked")
+        connected_users.pop(ip, None)
         
         messages["msgs"][channel].append(stringify_json(dc_msg))
         await broadcast_msg({
@@ -154,8 +127,7 @@ async def main(ws):
         })
         
     # returns true if the channel was craeted, false if it already exists
-    def new_channel(name, whitelist_enabled=False, whitelist=None):
-        whitelist = whitelist or []
+    def new_channel(name, whitelist_enabled=False, whitelist=[]):
         if name in config["channels"]:
             return False
         
@@ -170,64 +142,55 @@ async def main(ws):
         save_config()
         save_messages()
         return True
-    
-    async def init_session():
-        session = random_session()
-        try:
-            # key = base64.b64decode(msg["key"].encode())
-            # connected_users[ip]["key"] = key
-            await send_json({
-                "success": True, 
-                "session": session, 
-                "what": "session"
-            })
-            return session
-        
-        except Exception as e:
-            print(f"no session given because {e}")
-            await send_json({
-                "success": False, 
-                "what": "session",
-                "reason": "Client did not provide public key."
-            })
-            return None
 
     ip = ws.remote_address[0]
     
-    # none of this ip shit works rn
-    # if ip in config["banned_ips"]:
-    #     await send_json({"success": False, "reason": "We tolerate you no more.", "what": "connect"})
-    #     return
+    if ip in config["banned_ips"]:
+        await send_json({"success": False, "reason": "We tolerate you no more.", "what": "connect"})
+        return
     
-    # if any(user.ip == ip for user in connected_users):
-    #     await send_json({"success": False, "reason": "already connected", "what": "connect"})
-    #     return
-    
-    session = await init_session()
-    if not session:
-        await send_json({"success": False, "reason": "failed to get session", "what": "connect"})
-        return    
+    if ip in connected_users:
+        await send_json({"success": False, "reason": "already connected", "what": "connect"})
+        return
     
     await send_json({"success": True, "what": "connect"})
-    
-    curr_user = ConnectedUser(
-        session=session, 
-        ip=ip, 
-        socket=ws, 
-        loggedin=False, 
-        admin=False
-    )
-    anonymous_users.append(curr_user)
-    username = ""
+    connected_users[ip] = {
+        "websocket": ws,
+        "key": "",
+        "user": ""
+    }
     
     print(f"{ip} connected")
+    session = ""
+    loggedin = False
+    admin = False
+    username = ""
     
     try:
         async for raw_msg in ws:
             msg = json.loads(raw_msg)
             print(f"{ip} sent {msg}")
             
-            match msg.get("action", ""):                    
+            match msg.get("action", ""):
+                case "session":  # only happens at inital connect
+                    session = random_session()
+                    try:
+                        # key = base64.b64decode(msg["key"].encode())
+                        # connected_users[ip]["key"] = key
+                        await send_json({
+                            "success": True, 
+                            "session": session, 
+                            "what": "session"
+                        })
+                    
+                    except Exception as e:
+                        print(f"no session given because {e}")
+                        await send_json({
+                            "success": False, 
+                            "what": "session",
+                            "reason": "Client did not provide public key."
+                        })
+                    
                 case "login":
                     if await check_session(msg, "login", False):
                         continue
@@ -236,16 +199,10 @@ async def main(ws):
                         await send_json({"success": False, "reason": "Malformed request", "what": "login"})
                         continue
                     
-                    try:
-                        password = hashlib.sha256(bytes(msg["password"])).hexdigest()
-                    except Exception as e:
-                        print(f"bad password: {e}")
-                        await send_json({"success": False, "reason": "Malformed request", "what": "login"})
-                        continue
-                    
                     user = msg["username"]
+                    password = hashlib.sha256(bytes(msg["password"])).hexdigest().encode()
                     
-                    if get_user_info(user):
+                    if get_user_info(user) != {}:
                         await send_json({"success": False, "reason": "Already logged in", "what": "login"})
                         continue
                     
@@ -254,8 +211,7 @@ async def main(ws):
                         continue
                     
                     if user in users:
-                        if users[user][0] != password:
-                            print(users[user][0], password)
+                        if users[user]["pwd"] != password:
                             print("bad password")
                             
                             await send_json({"success": False, "reason": "Incorrect password", "what": "login"})
@@ -277,13 +233,12 @@ async def main(ws):
                     })
                     messages["msgs"][messages["channels"][0]].append(stringify_json(connected_msg))
                     
-                    curr_user.loggedin = True
+                    loggedin = True
+                    connected_users[ip]["user"] = user
                     username = user
-                    connected_users[user] = curr_user
-                    anonymous_users.remove(curr_user) 
                     
                     if user in config["admins"]:
-                        curr_user.admin = True
+                        admin = True
                         
                 case "data":
                     if await check_session(msg, "data"):
@@ -292,7 +247,7 @@ async def main(ws):
                     channels = get_whitelisted_channels(config, username)
                     print(f"sending messages to {ip} from these channels: {channels}")
                     data = last_n_msgs(channels, 1000)
-                    data["users"] = list(connected_users.keys())
+                    data["users"] = [user["user"] for _, user in connected_users.items()]
                     data["what"] = "data"
                     await send_json(data)
                     print("sent")
@@ -325,12 +280,12 @@ async def main(ws):
                         continue
                     
                     user_info = get_user_info(msg["user"])
-                    if not user_info:
+                    if user_info == {}:
                         continue
                     
-                    config["banned_ips"].append(user_info.ip)
+                    config["banned_ips"].append(user_info["ip"])
                     save_config()
-                    await kick(msg["user"])
+                    await kick(user_info["user"])
                     
                 case "unban":
                     if await check_session_admin(msg, "unban"):
@@ -345,11 +300,11 @@ async def main(ws):
                         continue
                     
                     user_info = get_user_info(msg["user"])
-                    if not user_info:
+                    if user_info == {}:
                         continue
                     
-                    if user_info.ip in config["banned_ips"]:
-                        config["banned_ips"].remove(user_info.ip)
+                    if user_info["ip"] in config["banned_ips"]:
+                        config["banned_ips"].remove(user_info["ip"])
                         save_config()
                     
                 case "dm":
@@ -360,16 +315,15 @@ async def main(ws):
                         continue 
                     
                     user_info = get_user_info(msg["user"])
-                    if not user_info:
+                    if user_info == {}:
                         continue
                     
-                    other = msg["user"]
+                    other = user_info["user"]
                     channel_name = f"{user} <-> {other} DM"
-                    payload = {"what": "newchannel", "channel_name": channel_name}
                     
                     if new_channel(channel_name, True, [user, other]):
-                        await curr_user.send_json(payload)
-                        await user_info.send_json(payload)
+                        await ws.send(json.dumps({"what": "newchannel", "channel_name": channel_name}))
+                        await user_info["websocket"].send(json.dumps({"what": "newchannel", "channel_name": channel_name}))
                     
                 case "newchannel":
                     if await check_session_admin(msg, "newchannel"):
@@ -378,17 +332,13 @@ async def main(ws):
                     channel_name = msg["channelname"]
                     if new_channel(channel_name):
                         await broadcast_msg({"what": "newchannel", "channel_name": channel_name})
-                    
+    
     finally:
-        if curr_user in anonymous_users:
-            anonymous_users.remove(curr_user)
-            return
-        
         channel = ws.close_reason
-        dc_msg = disconnect_msg(username)
+        dc_msg = disconnect_msg(connected_users[ip]["user"])
         
-        print(f"{username} disconnected")
-        connected_users.pop(username, None)
+        print(f"{ip} disconnected")
+        connected_users.pop(ip, None)
         
         if channel != "":
             messages["msgs"][channel or messages["channels"][0]].append(stringify_json(dc_msg))
@@ -396,32 +346,17 @@ async def main(ws):
                 "what": "new_msg",
                 "channel": channel,
                 "msg": json.dumps(dc_msg)
-            })  
+            })
+        
         
 async def _main():
-    certfile, keyfile = "cert.pem", "privkey.pem"
-    if "--localhost" in sys.argv:
-        certfile, keyfile = "localhost.pem", "localhost-key.pem"
-        
-    if not os.path.exists(certfile):
-        print(f"No certificate file found. Please create a new {certfile}")
-        return
-    
-    if not os.path.exists(keyfile):
-        print(f"No certificate file found. Please create a new {keyfile}")
-        return
-    
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    ssl_context.load_cert_chain(certfile=certfile, keyfile=keyfile)
-    ssl_context.verify_mode = ssl.CERT_NONE
-
     try:
         public_ip = requests.get('https://api.ipify.org').text
         print(f"public IP: {public_ip} (connect here as a client)")
     except:
         pass
 
-    async with websockets.serve(main, BIND_IP, PORT, ssl=ssl_context):
+    async with websockets.serve(main, "0.0.0.0", 2096):
         print("server started")
         try:
             await asyncio.Future()
@@ -432,9 +367,9 @@ async def kick_everyone():
     save_config()
     save_messages()
     save_users()
-    for username, data in connected_users.items():
-        print(f"kicking {username}")
-        await data.kick()
+    for ip, ws in connected_users.items():
+        print(f"kicking {ip}")
+        await ws["websocket"].close(code=1000, reason="internal error")
 
 try:
     if __name__ == "__main__":
